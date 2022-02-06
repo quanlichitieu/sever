ac327dab7568aa42e160ce6c03909fa081d2d8f0
lam <lam.2002.2015@gmail.com>
2022-02-06 20:25:48 +0700

    change http method of 3 trade api from get to post

--- src/router/trade.js
+++ src/router/trade.js
@@ -9,8 +9,8 @@ router.post('/addTrade', authorization, walletAuthorization, trade.addTrade)
 router.post('/updateTrade', authorization, walletAuthorization, trade.updateTrade)
 router.delete('/deleteTrade', authorization, walletAuthorization, trade.deleteTrade)
 router.delete('/deleteManyTrade', authorization, walletAuthorization, trade.deleteManyTrade)
-router.get('/getTrade', authorization, walletAuthorization, trade.getTrade)
-router.get('/getAllTrade', authorization, walletAuthorization, trade.getAllTrade)
-router.get('/getTradesByYear', authorization, walletAuthorization, trade.getTradesByYear)
+router.post('/getTrade', authorization, walletAuthorization, trade.getTrade)
+router.post('/getAllTrade', authorization, walletAuthorization, trade.getAllTrade)
+router.post('/getTradesByYear', authorization, walletAuthorization, trade.getTradesByYear)
 
 module.exports = router
